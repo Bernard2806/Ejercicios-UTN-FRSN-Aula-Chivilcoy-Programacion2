e6e4402a9adccf5e0feb9dcb98ff6c45bdfee6a3
Bernardo <bernard2806@users.noreply.github.com>
2025-08-08 00:02:31 +0000

    Subir primer programa de carga de Alumnos

---
+++ clase1_07_08/RegistroDeAlumnos/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) 
+    {
+        Alumno alumno = new Alumno();
+        
+        alumno.ingresarDatos();
+        alumno.mostrarDatos();
+        alumno.presentarse();
+    }
+}
