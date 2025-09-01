bdf17f1bcd0d3209466ea7caea8a7e4e821385cb
Bernardo <107374580+Bernard2806@users.noreply.github.com>
2025-09-01 18:54:34 -0300

    Mover de ubicación el archivo PDF y arreglar Main (App) Clase 7 (28/08)

--- clase7_28_08/Ejercicio/src/App.java
+++ clase7_28_08/Ejercicio/src/App.java
@@ -1,22 +1,30 @@
+import java.util.ArrayList;
+import Interfaces.Electrico;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        // Crear lista de instrumentos de la orquesta
+        ArrayList<Instrumento> orquesta = new ArrayList<>();
 
-        // Crear 2 instrumentos de cuerda y 2 de viento
+        // Agregar 5 instrumentos: guitarra acústica, violín, guitarra eléctrica, flauta, teclado
+        orquesta.add(new InstrumentoCuerda("Guitarra acústica", "Yamaha", 1200.00, 6));
+        orquesta.add(new InstrumentoCuerda("Violín", "Stradivarius", 5000.00, 4));
+        orquesta.add(new GuitarraElectrica("Guitarra eléctrica", "Fender", 1800.00, 6, false));
+        orquesta.add(new InstrumentoViento("Flauta", "Yamaha", 800.00, "Metal"));
+        orquesta.add(new TecladoElectronico("Teclado", "Casio", 1000.00, 61, false));
 
-        InstrumentoCuerda guitarra = new InstrumentoCuerda("Guitarra", "Fender", 1500.00, 6);
-        InstrumentoCuerda bajo = new InstrumentoCuerda("Bajo", "Gibson", 2000.00, 4);
-
-        InstrumentoViento saxofon = new InstrumentoViento("Saxofón", "Yamaha", 2500.00, "Caña");
-        InstrumentoViento trompeta = new InstrumentoViento("Trompeta", "Bach", 1800.00, "Latón");
-
-        // Los ponemos en un array
-        Instrumento[] orquesta = { guitarra, bajo, saxofon, trompeta };
-
-        // Lo recorremos con un Foreach y llamamos a los metodos
-
+        // Recorrer con for-each: mostrar info, tocar y afinar todos
         for (Instrumento instrumento : orquesta) {
             instrumento.mostrarInfo();
             instrumento.tocar();
+            instrumento.afinar();
+
+            // Funcionalidades específicas usando instanceof
+            if (instrumento instanceof Electrico) {
+                Electrico electrico = (Electrico) instrumento;
+                electrico.encender();
+                instrumento.tocar();
+            }
         }
     }
 }
